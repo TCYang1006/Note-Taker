e7372a5953bc477124354b853b467622e3274eea
Peter Thao <peterthao@Peters-iMac.local>
2021-11-09 18:51:31 -0500

    add post

--- Develop/server.js
+++ Develop/server.js
@@ -1,70 +1,87 @@
-const express = require('express');
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 const path = require('path');
 
+const express = require('express');
 const app = express();
-const PORT = 3001;
 
+const notes = require('./db/db.json');
 
-//Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extneded: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-
-//Routes
-//===============================================
-fs.readFile("db/db.json", "utf8", (err, data) => {
-
-    if (err) throw err;
-    //Data
-    var notes = JSON.parse();
-
-    app.get('/notes', (req, res) => {
-        const route = path.join(__dirname, '../public/notes.html');
-        res.sendFile(route);
-    });
-
-    app.get('*', (req, res) => {
-        const way = path.join(__dirname, '../public/index.html');
-        res.sendFile(way);
-    });
-
-
-    //TODO: create route to return all saved notes as JSON
-    app.get('/api/notes', (req, res) => {
-        res.json(notes);
-    });
-
-    //TODO: create route to save new notes
-    app.post('/api/notes', (req, res) => {
-        let newNote = req.body;
-        notes.push(newNote);
-        res.json(newNote);
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-            return;
-        });
-    });
-
-    //TODO: create route to get a single note from id
-    app.get('/api/notes/:id', (req, res) => {
-        //since human don't think of array starting in position zero
-        const chosen = (req.params.id - 1);
-        res.json(notes[chosen]);
-    });
-
-    //TODO: create route to delete a note from id
-    app.delete('/api/notes/:id', (req, res) => {
-        //since human don't think of array starting in position zero
-        const select = (req.params.id - 1);
-        notes.splice(select, 1);
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-            return;
-        });
-    });
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
 });
 
-//Listener
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+createNewNote(body, notesArray => {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+        if (chosen === notes[i]) {
+            return res.json(notes[i]);
+        }
+    }
+    return res.json(false);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id === id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true);
+});
+
 app.listen(PORT, () => {
-    console.log(`App listening on Port ${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
